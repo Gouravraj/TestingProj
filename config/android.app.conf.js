e8fcb09bf255be0f46e5916f49f1523f437b0541
Tony Jin <jsveron23@gmail.com>
2019-07-05 16:39:05 +0800

    CDP-666 | Add maxInstances option of config

--- config/android.app.conf.js
+++ config/android.app.conf.js
@@ -9,6 +9,7 @@ config.capabilities = [
     deviceName: 'emulator-5554',
     platformName: 'Android',
     orientation: 'PORTRAIT',
+    maxInstances: 1,
     app: join(process.cwd(), 'app', 'app-debug.apk'),
     newCommandTimeout: 240,
     autoGrantPermissions: true
