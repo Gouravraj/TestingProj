eecd5e474ee22cec976f4e1382897a0811d796d3
Tony Jin <jsveron23@gmail.com>
2019-07-09 16:19:05 +0800

    Edit mobile config files

--- config/mobile.android.config.js
+++ config/mobile.android.config.js
@@ -1,19 +1,19 @@
 const { join } = require('path');
 const { config } = require('./mobile.shared.config');
 
-config.specs = ['./tests/mobile-emp/specs/**/*.spec.js'];
-
-config.capabilities = [
-  {
-    automationName: 'UiAutomator2',
-    deviceName: 'emulator-5554',
-    platformName: 'Android',
-    orientation: 'PORTRAIT',
-    maxInstances: 1,
-    app: join(process.cwd(), 'app', 'app-debug.apk'),
-    newCommandTimeout: 240,
-    autoGrantPermissions: true
-  }
-];
-
-exports.config = { ...config };
+exports.config = {
+  ...config,
+  specs: ['./tests/mobile-emp/specs/**/*.spec.js'],
+  capabilities: [
+    {
+      automationName: 'UiAutomator2',
+      deviceName: 'emulator-5554',
+      platformName: 'Android',
+      orientation: 'PORTRAIT',
+      maxInstances: 1,
+      app: join(process.cwd(), 'app', 'app-debug.apk'),
+      newCommandTimeout: 240,
+      autoGrantPermissions: true
+    }
+  ]
+};
--- config/mobile.ios.config.js
+++ config/mobile.ios.config.js
@@ -1,20 +1,20 @@
 const { join } = require('path');
 const { config } = require('./mobile.shared.config');
 
-config.specs = ['./tests/mobile-emp/specs/**/*.spec.js'];
-
-config.capabilities = [
-  {
-    deviceName: 'iPhone X',
-    platformName: 'iOS',
-    platformVersion: '12.2',
-    orientation: 'PORTRAIT',
-    maxInstances: 1,
-    app: join(process.cwd(), 'app', 'app-debug.app'),
-    // useNewWDA: true,
-    waitForQuiescence: false,
-    automationName: 'XCUITest'
-  }
-];
-
-exports.config = { ...config };
+exports.config = {
+  ...config,
+  specs: ['./tests/mobile-emp/specs/**/*.spec.js'],
+  capabilities: [
+    {
+      deviceName: 'iPhone X',
+      platformName: 'iOS',
+      platformVersion: '12.2',
+      orientation: 'PORTRAIT',
+      maxInstances: 1,
+      app: join(process.cwd(), 'app', 'app-debug.app'),
+      // useNewWDA: true,
+      waitForQuiescence: false,
+      automationName: 'XCUITest'
+    }
+  ]
+};
