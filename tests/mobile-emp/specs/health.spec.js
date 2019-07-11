5def01eb8c5f0a21cbef9e333cad41105ffa92f7
scsannalli <scsannalli@gmail.com>
2019-07-11 14:07:43 +0800

    CDP-558|Enabling health tests on the ci|Shiva

--- tests/mobile-emp/specs/health.spec.js
+++ tests/mobile-emp/specs/health.spec.js
@@ -1,5 +1,5 @@
-// import health from '../actions/health.action';
-// import healthData from '../../data/mobile.health.data';
+import health from '../actions/health.action';
+import healthData from '../../data/mobile.health.data';
 import login from '../actions/login.action';
 import loginData from '../../data/mobile.login.data';
 
@@ -12,20 +12,20 @@ describe('Employee should,', () => {
     driver.reset();
   });
 
-  it('be able to submit health data', () => {
-    // expect(health.isHealthResult()).toBeTruthy();
-  });
+  // it('be able to submit health data', () => {
+  //   // expect(health.isHealthResult()).toBeTruthy();
+  // });
 
   it('be able to update health data', () => {
-    // health.updateHealthAs(healthData.updateHealthData);
-    // expect(health.isHealthResult()).toBeTruthy();
+    health.updateHealthAs(healthData.updateHealthData);
+    expect(health.isHealthResult()).toBeTruthy();
   });
 
-  it('be able to submit health data with Image', () => {
-    //ToDo :
-  });
-
-  it('be able to view Face aging details', () => {
-    //ToDo :
-  });
+  // it('be able to submit health data with Image', () => {
+  //   //ToDo :
+  // });
+  //
+  // it('be able to view Face aging details', () => {
+  //   //ToDo :
+  // });
 });
