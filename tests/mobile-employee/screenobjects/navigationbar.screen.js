373de42d1f2459d62687cef831487de6213a18a0
Patrick <patrick.m@cxagroup.com>
2019-07-15 15:42:16 +0800

    added additional credentials to use (and) updated navigation labels

--- tests/mobile-employee/screenobjects/navigationbar.screen.js
+++ tests/mobile-employee/screenobjects/navigationbar.screen.js
@@ -3,8 +3,8 @@ import AppScreen from './app.screen';
 const SELECTORS = {
   HEALTH_NAVIGATOR: '~Lifestyle, tab, 1 of 4',
   CLAIMS_NAVIGATOR: '~Claims, tab, 2 of 4',
-  SHOP_NAVIGATOR: '',
-  PROFILE_NAVIGATOR: ''
+  SHOP_NAVIGATOR: '~Choices, tab, 2 of 4',
+  PROFILE_NAVIGATOR: '~Me, tab, 4 of 4'
 };
 
 class NavigationBar extends AppScreen {
