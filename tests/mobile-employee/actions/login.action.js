7c8fc66d4c84d44ac6e437d1df3e930b67ca7f96
Nimit Jain <nimitjain31@gmail.com>
2019-07-22 21:50:50 +0530

    Removed testing function for login

--- tests/mobile-employee/actions/login.action.js
+++ tests/mobile-employee/actions/login.action.js
@@ -26,56 +26,6 @@ export function isLoggedin() {
   return isNavigationBarVisible();
 }
 
-export function loginAsNimit() {
-  LoginScreen.waitForIsShown(true);
-
-  LoginScreen.companyNameField().click();
-  LoginScreen.companyNameField().setValue('t');
-  LoginScreen.companyNameField().setValue('w');
-  LoginScreen.companyNameField().setValue('c');
-  LoginScreen.companyNameField().setValue('l');
-  LoginScreen.companyNameField().setValue('i');
-  LoginScreen.companyNameField().setValue('e');
-  LoginScreen.companyNameField().setValue('n');
-  LoginScreen.companyNameField().setValue('t');
-  LoginScreen.companyNameField().setValue('3');
-
-  LoginScreen.emailAddressField().click();
-  LoginScreen.emailAddressField().setValue('c');
-  LoginScreen.emailAddressField().setValue('x');
-  LoginScreen.emailAddressField().setValue('a');
-  LoginScreen.emailAddressField().setValue('t');
-  LoginScreen.emailAddressField().setValue('e');
-  LoginScreen.emailAddressField().setValue('s');
-  LoginScreen.emailAddressField().setValue('t');
-  LoginScreen.emailAddressField().setValue('2');
-  LoginScreen.emailAddressField().setValue('@');
-  LoginScreen.emailAddressField().setValue('t');
-  LoginScreen.emailAddressField().setValue('e');
-  LoginScreen.emailAddressField().setValue('s');
-  LoginScreen.emailAddressField().setValue('t');
-  LoginScreen.emailAddressField().setValue('.');
-  LoginScreen.emailAddressField().setValue('c');
-  LoginScreen.emailAddressField().setValue('o');
-  LoginScreen.emailAddressField().setValue('m');
-
-  // $(txt('Welcome to')).click();
-
-  LoginScreen.passwordField().click();
-  LoginScreen.passwordField().setValue('P');
-  LoginScreen.passwordField().setValue('@');
-  LoginScreen.passwordField().setValue('s');
-  LoginScreen.passwordField().setValue('s');
-  LoginScreen.passwordField().setValue('w');
-  LoginScreen.passwordField().setValue('0');
-  LoginScreen.passwordField().setValue('r');
-  LoginScreen.passwordField().setValue('d');
-
-  $(txt('Welcome to')).click();
-
-  LoginScreen.loginButton().click();
-}
-
 export function isLoginErrorMessageVisible() {
   let isVisible;
   const platform = driver.capabilities.platformName;
