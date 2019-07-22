ec95cc2e5fcc11432abcfa3d5dd16fd474a2b266
Anand <anand.kumar@thoughtworks.com>
2019-07-22 19:35:27 +0530

    CDP-1086 | added navigation bar automation | Anand

--- tests/web-employee/pageObjects/app.page.js
+++ tests/web-employee/pageObjects/app.page.js
@@ -1,6 +1,9 @@
 import { DEFAULT_TIMEOUT } from '../../constants';
 
 export default class Page {
+  constructor(selector) {
+    this.selector = selector;
+  }
   open(path) {
     browser.url(path);
   }
@@ -9,5 +12,15 @@ export default class Page {
     if ($(locator).isDisplayed()) {
       $(locator).waitForDisplayed(DEFAULT_TIMEOUT);
     }
+  }
+
+  /**
+   * Wait for the login screen to be visible
+   *
+   * @param  {boolean} isShown
+   * @return {boolean}
+   */
+  waitForIsShown(isShown = true) {
+    return $(this.selector).waitForDisplayed(DEFAULT_TIMEOUT, !isShown);
   }
 }
---
+++ tests/web-employee/specs/navigation.spec.js
@@ -0,0 +1,38 @@
+import { loginAs } from '../actions/login.action';
+import { validCredentials } from '../../data/login.data';
+import {
+  isNavigationBarVisible,
+  navigateToClaimsPage,
+  navigateToHealthPage,
+  navigateToShopPage,
+  navigateToProfilePage
+} from '../actions/navigator.action';
+describe('Employee should,', function() {
+  it('be able to login to employee web with valid credentials ', function() {
+    browser.pause(3000);
+    console.log('1 login');
+    loginAs(validCredentials);
+
+    browser.pause(3000);
+    console.log('2 login');
+    isNavigationBarVisible();
+
+    browser.pause(3000);
+    console.log('3 claim');
+    navigateToClaimsPage();
+
+    browser.pause(3000);
+    console.log('4 Health');
+    navigateToHealthPage();
+
+    browser.pause(3000);
+    console.log('5 choices');
+    navigateToShopPage();
+
+    browser.pause(3000);
+    console.log('6 me');
+    navigateToProfilePage();
+
+    browser.pause(3000);
+  });
+});
