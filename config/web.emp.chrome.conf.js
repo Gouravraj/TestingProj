ef6bf1034bbbe3f96e7404d4b0350e8f9d705256
Anand <anand.kumar@thoughtworks.com>
2019-07-22 12:57:31 +0530

    CDP-995 | Renamed the files for web-employee | Anand

--- config/web.emp.chrome.conf.js
+++ config/web.emp.chrome.conf.js
@@ -9,6 +9,6 @@ config.capabilities = [
   }
 ];
 
-config.baseUrl = 'https://distribution-employee-frontend.cxapalawandev.com/';
+config.baseUrl = 'https://benefits.cxapalawandev.com/';
 
 exports.config = { ...config };
