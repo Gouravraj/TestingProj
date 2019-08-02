b58f25e6426d5d30ea2dd98bf28dd463dc039b0d
scsannalli <scsannalli@gmail.com>
2019-08-02 10:38:46 +0800

    CDP-000 | Changing user credentials with new dev values|Shiva

--- tests/data/login.data.js
+++ tests/data/login.data.js
@@ -1,27 +1,27 @@
 // general test data (supports e-Health card)
 export const validCredentials = {
-  companyName: 'twclient3',
+  companyName: 'cxadevclient1',
   emailAddress: 'test3@test.com',
   password: 'P@ssw0rd'
 };
 
 // secondary test data (does not suppor e-Health card)
 export const validCredentials2 = {
-  companyName: 'twclient3',
+  companyName: 'cxadevclient1',
   emailAddress: 'cxatest2@test.com',
   password: 'P@ssw0rd'
 };
 
 // landing pages test data (Note: do not update anything on this test data)
 export const landingCredentials = {
-  companyName: 'twclient3',
+  companyName: 'cxadevclient1',
   emailAddress: 'cxadisttest001@test.com',
   password: 'P@ssw0rd'
 };
 
 // invalid test data credentials
 export const invalidCredentials = {
-  companyName: 'twclient33',
+  companyName: 'cxadevclient13',
   emailAddress: 'test3@test.com',
   password: 'P@ssw0rd'
 };
